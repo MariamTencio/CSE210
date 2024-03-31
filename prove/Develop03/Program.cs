01be34391ed8904219e6b13647f68ea944f6df04
MariamTencio <ten22011@byi.edu>
2024-03-30 20:51:46 -0600

    Push

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reff reference = new Reff("Proverbs",3, 5);
+        string text = " Trust in the Lord with all thine heart; and lean not unto thine own understanding.";
+        Scriptures scripture = new Scriptures(reference, text);
+        string quit = "quit";
+        Console.Clear();
+        scripture.Display();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            string input;
+            Console.Write("> ");
+            input = Console.ReadLine() ??"";
+            scripture.HideWords(1); 
+        while (input != quit) {
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            Console.Write("> ");
+            input = Console.ReadLine() ??"";
+            scripture.HideWords(3); 
+        }
+
     }
 }
