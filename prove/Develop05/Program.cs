9fa3a02d8c3faf738b968302e1e6924d34791c90
MariamTencio <ten22011@byi.edu>
2024-03-30 22:21:01 -0600

    push

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
+    private static List<Goal> _allGoals = new List<Goal>();
+    private static int total_points = 0;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
-    }
-}
+
+        
+        bool loop = true;
+        while (loop)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"You have {total_points} points.");
+            Console.WriteLine("");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Create a New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.Write("What would like to do? ");
+            int prompt = int.Parse(Console.ReadLine() ?? "");
+        }}}
